b5e167490b05814e725c6f7d1eb2b69d9e73bc36
p1r2i3n4c5e6 <girjawatiku@gmail.com>
2024-11-02 18:35:10 +0530

    total amount ui and functionality completely added

--- coffee_app_v2/lib/AddToCart.dart
+++ coffee_app_v2/lib/AddToCart.dart
@@ -43,35 +43,32 @@ class _AddtocartState extends State<Addtocart> {
       }
     });
   }
-    int get_total_itemcount(){
+
+  int get_total_itemcount() {
     int totalCount = 0;
-    for (var coffee in widget.cartItems){
+    for (var coffee in widget.cartItems) {
       totalCount += coffee.quantity;
     }
     return totalCount;
   }
 double convertPriceStringToDouble(String priceString) {
   try {
-    // Remove any currency symbols or commas if necessary
-    String sanitizedString = priceString.replaceAll(RegExp(r'[\$,]'), '');
+  
+    String sanitizedString = priceString.replaceAll(RegExp(r'[₹,\s]'), '');
     return double.parse(sanitizedString);
   } catch (e) {
     print("Error parsing price: $e");
-    return 0.0; // Return a default value in case of error
+    return 0.0; 
   }
 }
-double getTotalAmount() {
-  double totalAmount = 0.0;
-  for (var coffee in widget.cartItems) {
-    totalAmount += convertPriceStringToDouble(coffee.price) * coffee.quantity;
+
+  double getTotalAmount() {
+    double totalAmount = 0.0;
+    for (var coffee in widget.cartItems) {
+      totalAmount += convertPriceStringToDouble(coffee.price) * coffee.quantity;
+    }
+    return totalAmount;
   }
-  return totalAmount;
-}
-
-
-  
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -107,129 +104,126 @@ double getTotalAmount() {
                   fontSize: 20,
                   fontWeight: FontWeight.bold),
             ),
-            
             const SizedBox(height: 10),
             Expanded(
-              child:widget.cartItems.isEmpty
+              child: widget.cartItems.isEmpty
                 ? const Center(
                     child: Text(
                       'Your cart is empty!',
                       style: TextStyle(fontSize: 20),
                     ),
                   )
-                : 
-          
-                    ListView.builder(
-                      itemCount: widget.cartItems.length,
-                      itemBuilder: (context, index) {
-                        return buildCartCard(widget.cartItems[index], index);
-                      },
-                    ),
+                : ListView.builder(
+                    itemCount: widget.cartItems.length,
+                    itemBuilder: (context, index) {
+                      return buildCartCard(widget.cartItems[index], index);
+                    },
+                  ),
             ),  
-              Text(
-            'Total Amount: ${getTotalAmount().toStringAsFixed(2)}',
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),  
+        
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text(
+                'Total Amount: ₹ ${getTotalAmount().toStringAsFixed(2)}',
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-
-Widget buildCartCard(Coffee coffee, int index) {
-  return Card(
-    elevation: 5,
-    margin: const EdgeInsets.symmetric(vertical: 10),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(15),
-    ),
-    child: Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            height: 40,
-            width: 40,
-            child: Checkbox(
-              value: selectedItems[index],
-              shape: const CircleBorder(),
-              activeColor: Color(0xFF967259),
-              onChanged: (bool? value) {
-                setState(() {
-                  selectedItems[index] = value!;
-                });
-              },
-            ),
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.asset(
-              coffee.image,
-              height: 100,
-              width: 100,
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(coffee.name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 5),
-                Text(coffee.description, style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                                    
-                    Text(coffee.price, style: const TextStyle(fontSize: 14)),
-              
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Container(
-                          height: 30,
-                          width: 30,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Color(0xFF967259)),
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: IconButton(
-                            icon: const Icon(Icons.remove, size: 15),
-                            onPressed: () => _decrementQuantity(index),
-                            padding: EdgeInsets.zero, 
-                          ),
-                        ),
-                        const SizedBox(width: 5), 
-                        Text("${coffee.quantity}", style: const TextStyle(fontSize: 14)),
-                        const SizedBox(width: 5),
-                        Container(
-                          height: 30,
-                          width: 30,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Color(0xFF967259)),
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: IconButton(
-                            icon: const Icon(Icons.add, size: 15),
-                            onPressed: () => _incrementQuantity(index),
-                            padding: EdgeInsets.zero, 
-                          ),
-                        ),
-                      ],
-                    ),
-
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+  Widget buildCartCard(Coffee coffee, int index) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
       ),
-    ),
-  );
-}
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              child: Checkbox(
+                value: selectedItems[index],
+                shape: const CircleBorder(),
+                activeColor: Color(0xFF967259),
+                onChanged: (bool? value) {
+                  setState(() {
+                    selectedItems[index] = value!;
+                  });
+                },
+              ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                coffee.image,
+                height: 100,
+                width: 100,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(coffee.name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 5),
+                  Text(coffee.description, style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(coffee.price, style: const TextStyle(fontSize: 14)),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Color(0xFF967259)),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: IconButton(
+                              icon: const Icon(Icons.remove, size: 15),
+                              onPressed: () => _decrementQuantity(index),
+                              padding: EdgeInsets.zero, 
+                            ),
+                          ),
+                          const SizedBox(width: 5), 
+                          Text("${coffee.quantity}", style: const TextStyle(fontSize: 14)),
+                          const SizedBox(width: 5),
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Color(0xFF967259)),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: IconButton(
+                              icon: const Icon(Icons.add, size: 15),
+                              onPressed: () => _incrementQuantity(index),
+                              padding: EdgeInsets.zero, 
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
